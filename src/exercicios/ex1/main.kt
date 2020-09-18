10c9321321a70655722532b32bb38de748f07d0e
eneri-jr <eneri.jr@gmail.com>
2020-09-18 20:32:12 -0300

    alteracao exercicio 1

--- src/exercicios/ex1/main.kt
+++ src/exercicios/ex1/main.kt
@@ -1,15 +1,30 @@
 package exercicios.ex1
 
-class Pessoa(private val nome: String, private val rg: Int) {
+class Pessoa1(private val nome: String, private val rg: Int) {
         override fun equals(other: Any?): Boolean {
-        return other is Pessoa && rg == other.rg
+        return other is Pessoa1 && rg == other.rg
     }
 }
 
+class Pessoa2(private val nome: String, private val rg: Int) {
+    override fun equals(other: Any?): Boolean {
+        return other is Pessoa2 && nome == other.nome
+    }
+}
+
+
+
 fun main() {
-    val p1 = Pessoa("Rodrigo", 433033412)
-    val p2 = Pessoa("Rodrigo", 433033412)
+    val p1 = Pessoa1("Rodrigo", 433033412)
+    val p2 = Pessoa1("Rodrigo", 433033412)
 
     println(p1 == p2)
     println(p2.equals(p1))
+
+    val p3 = Pessoa2("Rodrigo", 433033412)
+    val p4 = Pessoa2("Rodrigo", 433033412)
+
+    println(p3 == p4)
+    println(p4.equals(p3))
+
 }
